294128f0e3c15e4136e359ac096d4d94b0b874e6
Hadassah Uche <uhadassah1@gmail.com>
2023-11-13 23:15:09 +0200

    This is the basic skeleton of the program

---
+++ UcheHadassah4_3/HadassahUche4_3.cpp
@@ -0,0 +1,26 @@
+/*a) Write a function called Count that takes as parameters an integer array a1, and 
+integer num, and returns the number of times num appears in the array a. You should 
+assume that there is a -1 at the end of the array. 
+b) Use your function Count above (i.e. call it) to write a function called IsMember
+that takes as parameters an integer array s1 (assumed to be terminated by -1) and an 
+integer x and returns true (i.e. 1) if x appears anywhere in the array. 
+Write a main program to test the function Count using a user defined array.*/
+#include<iostream>
+using namespace std;
+const int MAX = 100;
+int Count(int[], int);
+bool IsMember(int[], int);
+int main()
+{
+
+}
+
+int Count(int a1[], int num)
+{
+	return 0;
+}
+
+bool IsMember(int s1[], int x)
+{
+	return false;
+}
